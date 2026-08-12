056e2b264c2e8f3317e99b79c7281cf999cf0eb0
Dominick Robinson <drobinson.lbe@gmail.com>
2026-08-12 03:32:20 -0400

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.domonation.camera"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.2.1"
     }
 
     buildFeatures {
